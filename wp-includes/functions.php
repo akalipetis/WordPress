de0239d08cd6ddf066071178e4142addd6bb39cb
Antonis Kalipetis <akalipetis@gmail.com>
2015-12-18 12:33:18 +0200

    Fix functions.php to understand HTTPS header

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -4031,6 +4031,8 @@ function is_ssl() {
 			return true;
 	} elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
 		return true;
+	} elseif ( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+		return true;
 	}
 	return false;
 }
